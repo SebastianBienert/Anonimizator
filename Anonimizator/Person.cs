3b798908d1d9a887ea2b70c46a0a8c997726924c
sebastian_bienert <226086@student.pwr.edu.pl>
2019-10-09 16:47:15 +0200

    init

---
+++ Anonimizator/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonimizator
+{
+    public class Person
+    {
+        public string Gender { get; }
+
+        public string Job { get; }
+
+        public string City { get; }
+
+        public string FirstName { get;  }
+
+        public string Surname { get;  }
+
+    }
+}
